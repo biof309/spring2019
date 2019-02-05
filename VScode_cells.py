beb38daa19d65e45e01aac5482d08ff768951610
Martin Skarzynski <marskar@gmail.com>
2019-02-04 21:49:40 -0500

    add file

--- VScode_cells.py
+++ VScode_cells.py
@@ -1,2 +1,26 @@
-# %% map
-map(sum, [1,2,3])
+# %% plot
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
+
+
+
+#%% letters
+[chr(i+65) for i in range(26)]
+
+
+#%% 
