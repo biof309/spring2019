3a7ad3d75cf8e7047f6f47d0d9877e6947e4e0dc
Martin Skarzynski <marskar@gmail.com>
2019-02-12 13:57:55 -0500

    add files for 2nd class

---
+++ 20190212/README.md
@@ -0,0 +1,29 @@
+# Plan for 2019-02-05
+
+
+## Integrated Development Environments (IDEs)
+
+### Review
+- Make sure Anaconda (and VScode) and PyCharm are installed
+- Run code in PyCharm
+- Keyboard shortcuts in PyCharm
+
+### New material
+- Run code in VScode
+- VScode LiveShare
+- Keyboard shortcuts in VScode
+
+### Python basics
+
+### Review
+- Assignment
+- Types
+- List comprehensions
+
+### New material
+- Plotting with PyCharm and VScode
+- Dictionary[name, interest]
+- `difflib` for fuzzy matching
+- `pathlib` for reading/writing files
+
+
---
+++ 20190212/assign.py
@@ -0,0 +1,41 @@
+# Assign more than one variable at a time
+x = y = z = 1
+
+# Assign more than one variable at a time using tuple unpacking
+values = 1, 2, 3
+one, two, three = values
+
+# Assign more than one variable at a time from a list comprehension
+one, two, three = [x+1 for x in range(3)]
+
+# Assign more than one variable at a time
+about_me = 'Martin', 'Skarzynski', 1985, 'Warsaw', 'Poland'
+firstname, lastname, birthyear, birthcity, birthcountry = about_me
+first, last, *birth = about_me
+first, last, *birth, nationality = about_me
+
+# A string is a sequence of characters
+a, b, c = 'a', 'b', 'c'
+a, b, c = 'abc'
+
+# Swap two variables
+a, b = 'a', 'b'
+a, b = b, a
+a, b
+
+# Pair up keys and values to make a dictionary
+vals = 'Martin', 'Skarzynski', 1985, 'Warsaw', 'Poland'
+keys = 'firstname', 'lastname', 'birthyear', 'birthcity', 'birthcountry'
+d = dict(zip(keys, vals))
+
+# Add more than one key-value pair to a dictionary at a time
+d['job', 'hobby'] = 'scientist', 'zumba'
+
+nums = 1, 2, 3, 4
+
+# Unpack two tuples into a list
+[*vals, *keys]
+
+# If you want to do something fancy, use a dictionary comprehension
+d2 = {'My ' + key: 'is ' + str(value) for key, value in zip(keys, vals)}
+d2
